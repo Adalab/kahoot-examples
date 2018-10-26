a2af714099233028807d03dde5fd8eeec977ec68
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-10-26 11:15:04 +0200

    Add 2.13 examples

---
+++ sprint-2/s2_13.js
@@ -0,0 +1,123 @@
+//Question 1
+
+const mood = ':)';
+
+function showMood(){
+  const mood = ':(';
+  console.log(mood);
+}
+
+console.log(mood);
+
+
+//Question 2
+
+const mood = ':)';
+
+function showMood(){
+  const mood = ':(';
+  console.log(mood);
+}
+
+showMood();
+console.log(mood);
+
+
+//Question 3
+
+const mood = ':)';
+
+function showMood(){
+  console.log(mood);
+}
+
+showMood();
+console.log(mood);
+
+//Question 4
+
+const mood = ':)';
+
+function showMood(){
+  mood = ':(';
+  console.log(mood);
+}
+
+showMood();
+console.log(mood);
+
+
+//Question 5
+
+function showMood(){
+  const mood = ':(';
+  console.log(mood);
+}
+
+showMood();
+console.log(mood);
+
+
+//Question 6
+
+const mood = ':)';
+
+showMood();
+console.log(mood);
+
+function showMood(){
+  const mood = ':(';
+  console.log(mood);
+}
+
+
+//Question 7
+
+showMood();
+console.log(mood);
+
+const mood = ':)';
+
+function showMood(){
+  const mood = ':(';
+  console.log(mood);
+}
+
+
+//Question 8
+
+const mood = ':)';
+
+showMood();
+console.log(mood);
+
+const showMood = () => {
+  const mood = ':(';
+  console.log(mood);
+}
+
+
+//Question 9
+
+const mood = ':)';
+
+const showMood = () => {
+  const mood = ':(';
+  console.log(mood);
+}
+
+showMood();
+console.log(mood);
+
+
+//Question 10
+
+const mood = ':)';
+console.log(mood);
+
+const showMood = () => {
+  const mood = ':(';
+  console.log(mood);
+}
+showMood();
+
