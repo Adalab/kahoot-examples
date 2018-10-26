6d2857d0509983129ea5fd41c55f07b5b2c7a867
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-10-26 12:42:19 +0200

    Add questions for session 2.11

---
+++ sprint-2/s2_11.js
@@ -0,0 +1,14 @@
+//Question 8
+
+const nameInput = document.querySelector('#firstname');
+
+console.log(nameInput.value);
+nameInput.value = 'Joan';
+
+//Question 9
+
+const label = document.querySelector('.firstname-label');
+const value = label.getAttribute('for');
+
+console.log(value);
+
