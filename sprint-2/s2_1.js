b0445b1670cd653cc65b82802ae54e31f7f95681
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-10-29 16:28:12 +0100

    Questions of session 2.1

---
+++ sprint-2/s2_1.js
@@ -0,0 +1,20 @@
+//Question 5
+document.querySelector('h1').innerHTML='¡Hola mundo!';
+
+//Question 6 & 7
+let officeAddress = 'Calle Leganitos, 24';
+
+//Question 8
+let earnings = 10;
+let expenses = 8;
+let benefits = earnings - expenses;
+
+//Question 9
+const sections = 4;
+const sectionsSpace = 20 * sections;
+sections = 5;
+
+//Question 10
+const sectionA = document.querySelector('.section-a');
+
+sectionA.classList.add('hidden');
